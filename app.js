94ac3d56dc5f65842f63959d7d98775070693aad
Charly Nonorgue <charlynonorgue88@gmail.com>
2021-02-23 14:44:49 +0100

    routes user ok, login et signup ok

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+const path = require('path');
 
 mongoose
   .connect(
@@ -28,8 +30,9 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use('/api/sauces', sauceRoutes);
-
-
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- models/user.js
+++ models/user.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    email: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+    email: {type: String, required: true},
     password : {type: String, required: true},
 });
 
-module.exports = mongoose.model('user', userSchema);
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
